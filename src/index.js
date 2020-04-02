7f689fa98721df82076929a9ab2da18488d3aac3
Abdelkabiir <abdelkabir.watil@gmail.com>
2020-04-02 21:02:07 +0100

    add sort function

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import {CollectionNames} from "./constants";
 import {SubCategoriesData} from "./utils/sub-categories-data";
 import {Names} from "./utils/names-data";
 import {Centuries} from './utils/centuries';
+import {sortArray} from "./utils/sort";
 
+/**
+ * Uncomment the part of the code you want to use.
+ */
 // updateCollection(CollectionNames.SubCategoryDetails ,SubCategoriesData)
 // console.log(Names.length);
-// multipleUpdateCollection(CollectionNames.Centuries, Centuries)
+/* 
+Centuries.forEach(century => {
+    sortArray(century.scholars);
+})
+multipleUpdateCollection(CollectionNames.Centuries, Centuries)
+*/
 
 
